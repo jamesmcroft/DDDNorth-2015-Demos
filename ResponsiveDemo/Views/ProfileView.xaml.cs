12957aae543f28a468ff2b1e9810b3ef7c777599
James Croft <jamz_c@hotmail.co.uk>
2015-10-20 22:40:42 +0100

    Added missing files

---
+++ ResponsiveDemo/Views/ProfileView.xaml.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProfileView.xaml.cs" company="James Croft">
+//   Copyright (c) 2015 James Croft. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the ProfileView type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ResponsiveDemo.Views
+{
+    /// <summary>
+    /// The profile view.
+    /// </summary>
+    public sealed partial class ProfileView
+    {
+        public ProfileView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ ResponsiveDemo/Views/SettingsView.xaml.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SettingsView.xaml.cs" company="James Croft">
+//   Copyright (c) 2015 James Croft. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the SettingsView type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ResponsiveDemo.Views
+{
+    /// <summary>
+    /// The settings view.
+    /// </summary>
+    public sealed partial class SettingsView
+    {
+        public SettingsView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
